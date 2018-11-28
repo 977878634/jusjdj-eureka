262daf7a93ce23fb075e8d36e4e0484b30c82175
justdj <977878634@qq.com>
2018-11-28 17:36:20 +0800

    修改打包鏡像的配置

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,5 @@
 echo "开始构建docker镜像"
 mvn package docker:build -Dmaven.test.skip=true
 #上传到docker服务器
-docker login --username=13588224627 registry-internal.cn-qingdao.aliyuncs.com
-docker tag test registry-internal.cn-qingdao.aliyuncs.com/justdj/shan:latest
-docker push registry-internal.cn-qingdao.aliyuncs.com/justdj/shan:latest;;
+sudo docker login --username=13588224627 registry.cn-qingdao.aliyuncs.com
+sudo docker push registry.cn-qingdao.aliyuncs.com/justdj/eureka:latest
